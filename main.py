82dc83314afcf1a5a9ba6426195655e3c33956f7
Kingsley Ihemere <dekings44@gmail.com>
2022-07-12 23:24:53 +0100

    first commit quiz

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class User:
+
+    def __int__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
+user_1 = User('001', 'Kings')
+
+user_2 = User('002', 'Chinaza')
+
+print(user_1)
+print(user_2)
