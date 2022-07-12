3a78be5dd73a173bae385306924e62579d197ccc
Kingsley Ihemere <dekings44@gmail.com>
2022-07-12 23:35:42 +0100

    first commit quiz

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ class User:
     def __int__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+
 
 user_1 = User('001', 'Kings')
 
 user_2 = User('002', 'Chinaza')
 
-print(user_1)
-print(user_2)
+print(user_1.id)
+#print(user_2)
